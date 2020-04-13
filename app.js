76088c321896be68a6d8a3ec8d4c6b82b856b90b
xuanngo10 <xuan.ngo10@gmail.com>
2020-04-12 22:21:45 -0700

    progress

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 const express = require("express");
 const app = express();
 
-const data = require("data.json");
+// require the data file
+const data = require("./data/data.json").projects;
 
 // View Engine Setup
 app.set("view engine", "pug");
@@ -19,10 +20,32 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/projects", (req, res) => {
-  res.render("project");
+app.get("/projects/:id", (req, res) => {
+  res.render("project", {
+    projectId: data[req.params.id].id,
+    projectName: data[req.params.id].project_name
+  });
 });
 
+// Error handles
+app.use((req, res, next) => {
+  const err = new Error("oh no!");
+  err.status = 500;
+  next(err);
+});
+
+app.use((req, res, next) => {
+  // Log statement to indicate that this function is running
+  console.log("Handling 404 error");
+
+  const err = new Error(
+    "Oops, page not found. Looks like that route does not exist."
+  );
+  err.status = 404;
+  next(err);
+});
+
+// Turn on Express server
 app.listen(3000, () => {
   console.log("The application is running on localhost:3000!");
 });
